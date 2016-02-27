1da142835722f2d2edc1985aa2467abb08f6429d
Marcin <dlugopix@gmail.com>
2016-02-27 20:00:23 +0100

    +added route for places

--- Backend/app.py
+++ Backend/app.py
@@ -1,5 +1,7 @@
+import json
+from wsgiref import simple_server
+
 from flask import Flask
-from apiclient.http import BatchHttpRequest
 
 app_url = ''
 app = Flask(__name__)
@@ -8,5 +10,11 @@ app.debug = True
 
 @app.route(app_url + '/places/')
 def get_places():
-    pass
-    return "Czy sie komintelo"
+    obj = {}
+    obj['miejsce'] = 'Warszawa'
+    obj['nazwa'] = 'bar'
+    return json.dumps(obj)
+
+if __name__ == '__main__':
+    httpd = simple_server.make_server('127.0.0.1', 5000, app)
+    httpd.serve_forever()
