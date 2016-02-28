d3b61ce2c1f410d6d027b2870d408fd717946822
bartmin <bartminm@ee.pw.edu.pl>
2016-02-28 11:24:24 +0100

    Added Handlebars for dynamically loading templates - working!

--- Frontend/js/main.js
+++ Frontend/js/main.js
@@ -129,7 +129,21 @@ function addMarker(place) {
     });
 }
 
-
+Handlebars.getTemplate = function(name) {
+    if (Handlebars.templates === undefined || Handlebars.templates[name] === undefined) {
+        $.ajax({
+            url : 'templates/' + name + '.handlebars',
+            success : function(data) {
+                if (Handlebars.templates === undefined) {
+                    Handlebars.templates = {};
+                }
+                Handlebars.templates[name] = Handlebars.compile(data);
+            },
+            async : false
+        });
+    }
+    return Handlebars.templates[name];
+};
 
 
 
