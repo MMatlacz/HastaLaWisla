6a8a463d0c8975de6c48c4ae5820423acb527cdd
jahooo <okraskaj@ee.pw.edu.pl>
2016-02-28 11:27:54 +0100

    dodane pierwsze miejsce

---
+++ Backend/miejscaJanek.py
@@ -0,0 +1,5 @@
+from app import Miejsce
+
+HockiKlocki = Miejsce("HckKlck","Hocki Klocki","52.232311" ,"21.041103","muzyka","Założeniem klubu jest powrót do krainy dzieciństwa. Hocki Klocki znajdują się przy Bulwarze Flotylli Wiślanej, w niedalekim sąsiedztwie ulicy Ludnej.W otoczeniu drzew i z kawałkiem plaży- idealna przestrzeń do wakacyjnego relaksu.","0.jpg","1.jpg","2.jpg","3.jpg","1","11:00","2:00")
+
+#identyfikator, nazwa,long, lat,kategoria, opis,zdjecie_main,zdj1,zdj2,zdj3,cena, otwarcie, zamkniecie
